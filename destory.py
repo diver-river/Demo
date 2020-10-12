83e30346f480f1de29dd1f4403fa413fd7405368
Zyj-Eternal <zyj987324339@163.com>
2020-10-12 21:35:22 +0800

    destory

---
+++ destory.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+#########################################################################
+# -*- coding: utf-8 -*-
+# File Name: destory.py
+# Author :kite
+# QQ : 987324339
+# Email:987324339@qq.com
+# Created Time: Mon 12 Oct 2020 09:34:57 PM CST
+#########################################################################
+
+def destory():
+    print('you lose')
